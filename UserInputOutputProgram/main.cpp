c76dea754d73479c93b245a02c4a098c20e37447
mattd982 <54785482+mattd982@users.noreply.github.com>
2021-01-11 20:09:21 -0500

    Add files via upload
    Learn how to write code that has the terminal ask for a number and adds the sum of two numbers, using variables.

---
+++ UserInputOutputProgram/main.cpp
@@ -0,0 +1,30 @@
+//Program with user input: Addition of two numbers
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    
+    
+    int A;
+    int B;
+    int sum;
+    
+    cout<<"Please enter first number=";
+    cin>>A;
+    
+    cout<<"Please enter second number=";
+    cin>>B;
+    
+    sum=A+B;
+    
+    cout<<"Sum of two numbers A and B="<<sum;
+    
+    
+    
+    
+    
+    
+}
+
