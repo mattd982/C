c57948d934924140cdb6e7e15b151edac7da25f0
mattd982 <54785482+mattd982@users.noreply.github.com>
2021-01-12 12:44:30 -0500

    Add files via upload
    Writing if statements and else statements in C++. Have the console output "even" if a number is even or output "odd" if the number is odd.

---
+++ 6.Statements/main.cpp
@@ -0,0 +1,28 @@
+// if statement: Example
+/*
+ if(condition){
+    condition is true :code
+ }else{
+    when condition is false :code
+ }
+ */
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    
+    int a;
+    cout<<"Please enter value=";
+    cin>>a;
+    
+    if(a%2==0){
+        cout<<"Number is even";
+    }else{
+        cout<<"Number is odd";
+        
+    }
+    
+    return 0;
+}
+
