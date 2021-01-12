976bc053e9d950b7c7f4cf9a7123ce2b771ddefe
mattd982 <54785482+mattd982@users.noreply.github.com>
2021-01-11 19:44:52 -0500

    Add files via upload
    Intro to C++, learning how to push file to github

---
+++ C++ Programming/Variables/main.cpp
@@ -0,0 +1,41 @@
+// This is a single line comment 
+/* This is multiple line comment
+ * C++ supports the object-oriented programming
+ * Standard C++ programming is divided into three parts
+ * 1. The core library includes the data types, variables and literals, etc.
+ * 2. The standard library includes the set of functions manipulating strings, files, etc.
+ * 3. The standard template library(STL) includes the set of methods manipulating a data structure.
+ * 
+ * Lines beginning with a hash sign (#) are directives for the preprocessor.
+ * They are not regular code lines with expressions but indications for the compiler's preprocessor.
+ * Using: you are going to use it
+ * Variables cannot start with any special characters or digits except underscores
+ 
+ */
+
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    
+    int a;
+    int a10;
+    int _a;
+    
+    a=10;
+    a10=50;
+    _a=600;
+    
+    cout<<a<<endl;
+    cout<<a10<<endl;
+    cout<<_a<<endl;
+    
+    cout<<"a="<<a<<endl;
+    cout<<"a10="<<a10<<endl;
+    cout<<"_a="<<_a<<endl;
+    
+    
+    
+    
+}
