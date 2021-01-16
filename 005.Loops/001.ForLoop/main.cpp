01213f552417baad3d64b7c831af8183dbe637a4
mattd982 <54785482+mattd982@users.noreply.github.com>
2021-01-16 14:44:48 -0500

    Add files via upload
    001. Have console display numbers 1 to 10 using a loop.
    002. The console will print a set of numbers in the terminal using while loop.
    003. Have the console output numbers 1 to 15 using do while loop.

---
+++ 005.Loops/001.ForLoop/main.cpp
@@ -0,0 +1,29 @@
+//For loop
+/*
+ Three parts of for loop(initialization; condition; increment/decrement
+ */
+//Program to print numbers from 1 to 10
+//Program to print numbers from 10 to 1
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    int i;
+    for(i=1; i<=10; i++){
+        cout<<i<<endl;
+    }
+    return 0;
+}
+
+
+   /*#include<iostream>
+   using namespace std;
+  
+   int main(){
+   int i;
+   for(i=10; i>=1; i--){
+       cout<<i<<endl;
+    }
+    return 0;
+}*/
---
+++ 005.Loops/003.DoWhileLoop/main.cpp
@@ -0,0 +1,26 @@
+//do while loop:
+/*
+ do{
+   some code
+ }
+ while(condition);
+ * 
+ * while loop
+ * while(condition);
+ 
+ */
+//print numbers using do while
+#include<iostream>
+using namespace std;
+
+int main(){
+    
+    int n=1;
+    do{
+        cout<<n<<endl;
+        n++;
+    
+    }while(n<=15);
+    
+    return 0;
+}
